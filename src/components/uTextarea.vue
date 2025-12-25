03b32ee34064f949207310bacae66168a275b24b
Dmitry Makarov <makdmt@gmail.com>
2025-12-25 18:00:32 +0300

    feat(components): add uTextarea

---
+++ src/components/uTextarea.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+defineProps<{
+  modelValue: string | undefined | null
+}>()
+
+const emit = defineEmits<{
+  (event: 'update:modelValue', value: string | undefined | null): void
+}>()
+
+const onInput = (evt: InputEvent) => {
+  if (evt.target instanceof HTMLTextAreaElement) emit('update:modelValue', evt.target.value);
+}
+</script>
+
+
+<template>
+    <textarea
+        v-bind="$attrs"
+        class="textarea"
+        :value="modelValue"
+        @input="onInput"
+    />
+</template>
+
+<style lang="scss">
+
+.textarea {
+  @include input-field;
+
+  &:focus {
+    @include focus-state;
+  }
+}
+
+</style>
+
