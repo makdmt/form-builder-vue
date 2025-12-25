052cc6d79844bb818772bc31baa9da23856ae0f2
Dmitry Makarov <makdmt@gmail.com>
2025-12-25 18:00:13 +0300

    feat(components): add uInput

---
+++ src/components/uInput.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+
+defineProps<{
+  modelValue: string | undefined | null
+}>()
+
+const emit = defineEmits<{
+  (event: 'update:modelValue', value: string | undefined | null): void
+}>()
+
+const onInput = (evt: InputEvent) => {
+  if (evt.target instanceof HTMLInputElement) emit('update:modelValue', evt.target.value);
+}
+
+</script>
+
+<template>
+  <input
+      v-bind="$attrs"
+      class="input"
+      :value="modelValue"
+      @input="onInput"
+  />
+</template>
+
+<style lang="scss">
+
+.input {
+  @include input-field;
+
+  &:focus {
+    @include focus-state;
+  }
+}
+
+</style>
