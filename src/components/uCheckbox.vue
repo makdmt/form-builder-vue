d03ffb29fb5f49e1f38e237782ef6097302ae811
Dmitry Makarov <makdmt@gmail.com>
2025-12-25 18:00:51 +0300

    feat(components): add uCheckbox

---
+++ src/components/uCheckbox.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+
+defineProps<{
+  modelValue: boolean
+}>()
+
+const emit = defineEmits<{
+  (event: 'update:modelValue', value: boolean): void
+}>()
+
+const onChange = (evt: Event) => {
+  if (evt.target instanceof HTMLInputElement) emit('update:modelValue', evt.target.checked);
+}
+
+</script>
+
+<template>
+  <input
+      v-bind="$attrs"
+      type="checkbox"
+      class="checkbox"
+      :checked="modelValue"
+      @change="onChange"
+  />
+</template>
+
+<style lang="scss">
+
+.checkbox {
+  min-width: 18px;
+  min-height: 18px;
+  accent-color: white;
+  cursor: pointer;
+  margin: 0;
+  outline: 1px solid #ccc;
+
+  &:focus-visible {
+    outline: 1px solid $highlight-color;
+  }
+}
+
+</style>
