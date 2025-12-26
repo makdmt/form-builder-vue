32c5cb48c7a856e02c0dabb0957be59f18b80b83
Dmitry Makarov <makdmt@gmail.com>
2025-12-26 15:09:06 +0300

    feat(components): add uCard

---
+++ src/components/uCard.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="card">
+    <slot/>
+  </div>
+</template>
+
+<style lang="scss">
+.card {
+  background: #fff;
+  border: 1px solid #e4e4e4;
+  border-radius: 12px;
+  padding: 20px;
+}
+</style>
